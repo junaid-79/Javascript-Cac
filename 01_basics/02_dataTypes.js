e8b1159051596e7d86cec275cac6931ef737919c
MOHAMMED JUNAID AHAMMED <121958406+junaid-79@users.noreply.github.com>
2024-09-19 16:57:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+
+let age = 20;
+let value = 22.65;
+
+let name = "junaid"
+
+let isFollow = true;
+
+let val = null;
+
+let a;
+
+console.log(age);
+console.log(value);
+console.log(name);
+console.log(isFollow);
+console.log(val);
+console.log(a);
+
+console.log(typeof age);
+console.log(typeof value);
+console.log(typeof name);
+console.log(typeof isFollow);
+console.log(typeof val);
+console.log(typeof a);
