c657e4fabd27ef56a88159c3ed8b7749eecbd26a
MOHAMMED JUNAID AHAMMED <121958406+junaid-79@users.noreply.github.com>
2024-09-21 06:46:35 +0000

    Datatype Conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -62,3 +62,48 @@ let stringNum = String(num);
 
 console.log(stringNum);
 console.log(typeof stringNum);
+
+
+
+// **********************************Operations*************************************************
+
+let val = 3;
+let negVal = -val
+console.log(negVal)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+
+let str1 = "junaid";
+let str2 = "ahammed";
+
+
+let str = str1 + str2
+console.log(str);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++; // ++gameCounter;
+console.log(gameCounter);
