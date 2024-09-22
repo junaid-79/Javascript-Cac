e72bd149ab9a0993e9f6ad981b0f5118b2ffd079
MOHAMMED JUNAID AHAMMED <121958406+junaid-79@users.noreply.github.com>
2024-09-22 13:19:56 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1)
+// console.log(1 < 3)
+// console.log(1 <= 1)
+// console.log(2 >= 4)
+// console.log(1 != 2)
+// console.log(1 == 1)
+
+
+console.log("22" > 11)
+console.log("02" > 1)
+
+
+// console.log(null > 0) // null converts to zero
+// console.log(null == 0)
+// console.log(null >= 0) // null converts to zero
+
+
+// console.log(undefined > 0) // undefined always results false
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+
+
+// we always try to avoid such type of conversions we always try to write a clean code 
+
+
+// ===  it checks value and also datatype
+
+console.log(2 === 2)
+console.log("2" === 2);
