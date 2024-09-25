9874ef843db32438705c7b626a511ebe7e83950f
MOHAMMED JUNAID AHAMMED <121958406+junaid-79@users.noreply.github.com>
2024-09-25 15:10:53 +0000

    Conditional Statements

---
+++ 01_basics/ConditionalStatements.js
@@ -0,0 +1,71 @@
+// to implement some condition in code
+
+// if statement
+
+// let age = 15;
+
+// if (age > 18) {
+//     console.log("you can vote");
+// }
+
+// if(age < 18) {
+//     console.log("you can not vote");
+// }
+
+let mode = "dark";
+let color;
+
+// if(mode === "dark") {
+//     color = "dark";
+// }
+
+// if(mode === "white") {
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+// if-else statement
+
+if(mode === "dark") {
+    color = "dark";
+} else {
+    color = "white";
+}
+
+console.log(color);
+
+// let age = 25;
+
+// if(age >= 18) {
+//     console.log("Vote");
+// } else {
+//     console.log("Cant Vote");
+// }
+
+// // odd or even
+// let num = 10;
+
+// if (num % 2 == 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }
+
+
+// else-if statement
+
+let age = 25;
+
+if (age < 18) {
+    console.log("Minor");
+} else if (age < 40) {
+    console.log("Major");
+} else {
+    console.log("Senior");
+}
+
+
+
+
